31ade752aeaf177ab028984e3edfcde0528785df
Gustavo Cotta <gmcotta34@gmail.com>
2019-08-11 19:43:38 -0300

    Adicionando a função 'It's a match' no mobile

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import {YellowBox} from 'react-native';
 import Routes from './routes';
+
+YellowBox.ignoreWarnings([
+  'Unrecognized WebSocket',
+]);
 
 function App() {
   return (
     <Routes />
   );
-}
+};
 
 export default App;
