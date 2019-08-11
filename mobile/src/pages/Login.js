a133d191f93968c21a8038fcf2fd5dbc53d66cb4
Gustavo Cotta <gmcotta34@gmail.com>
2019-08-11 18:34:02 -0300

    Parte mobile da aplicação criada

---
+++ mobile/src/pages/Login.js
@@ -0,0 +1,93 @@
+import React, {useState, useEffect} from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import {KeyboardAvoidingView, Platform, StyleSheet, Image, Text, TextInput, TouchableOpacity} from 'react-native';
+
+import api from '../services/api';
+
+import logo from '../assets/logo.png';
+
+export default function Login({navigation}) {
+    const [user, setUser] = useState('');
+
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user => {
+            if (user) {
+                console.log('Passou pelo async storage');
+                navigation.navigate('Main', {user});
+            }
+        });
+    }, []);
+
+    async function handleLogin() {
+        //console.log(user);
+
+        const response = await api.post('/devs', {
+            username: user
+        });
+
+        const {_id} = response.data;
+
+        //console.log(_id);
+
+        await AsyncStorage.setItem('user', _id);
+
+        navigation.navigate('Main', {user: _id});
+    }
+
+    return (
+        <KeyboardAvoidingView 
+            style={styles.container}
+            behavior='padding'
+            enabled={Platform.OS === 'ios'}>
+            <Image source={logo} />
+            <TextInput 
+                placeholder='Digite seu usuário no GitHub'
+                placeholderTextColor='#999'
+                autoCorrect={false}
+                style={styles.input}
+                value={user}
+                onChangeText={setUser} />
+            <TouchableOpacity 
+                style={styles.button}
+                onPress={handleLogin}>
+                <Text style={styles.buttonText}>Enviar</Text>
+            </TouchableOpacity>
+                
+        </KeyboardAvoidingView>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f5f5f5',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 30,
+    },
+    input: {
+        height: 46,
+        alignSelf: 'stretch',
+        backgroundColor: '#fff',
+        borderWidth: 2,
+        borderColor: '#ddd',
+        borderRadius: 4,
+        marginTop: 20,
+        paddingHorizontal: 15,
+    },
+    button: {
+        height: 46,
+        alignSelf: 'stretch',
+        backgroundColor: '#df4723',
+        borderRadius: 4,
+        marginTop:10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 16,
+    }
+
+});
